90cae44415b05b6bfe5c2126c683b2adf9778f9d
Matteo Moscardini <160152042+matte-mosca@users.noreply.github.com>
2024-04-09 17:47:13 +0200

    correct

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const form = document.querySelector("form");
 const saveBtn = document.getElementById("save-btn");
 const resetBtn = document.getElementById("reset-btn");
+const inputUsername = document.getElementById("inputUsername")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault(); 
 
   const name = inputUsername.value;
   localStorage.setItem("user", name);
-  users.innerHTML = `Nome di accesso: ${username}`;
+  users.innerHTML = `Nome di accesso: ${name}`;
   form.reset();
 });
 
